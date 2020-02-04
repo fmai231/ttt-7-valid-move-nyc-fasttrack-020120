8e587257e930f7f6fad3ab22c711a7a439ae9934
Franky Mai <frankymai91@gmail.com>
2020-02-04 20:28:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,20 +1,20 @@
 
 
-def valid_move?(board,index)
-  if board[100] == ""
-    puts "board 100"
-     false
-  elsif board[4] == ""
-    puts "board 4"
-     nil
-  elsif board[0] == " "
-    puts "board 0"
-     true
-  elsif board[5] == " "
-    puts "board 5"
-     true
-     end
-end
+# def valid_move?(board,index)
+#   if board[100] == ""
+#     puts "board 100"
+#     false
+#   elsif board[4] == ""
+#     puts "board 4"
+#     nil
+#   elsif board[0] == " "
+#     puts "board 0"
+#     true
+#   elsif board[5] == " "
+#     puts "board 5"
+#     true
+#     end
+# end
 
 
 def valid_move?(board,index) ## check if position is valid and not taken
